2c9235be0b446176b31fc105cf6217ce315c42b9
jusimigil <jose_miguel_octavo@dlsu.edu.ph>
2026-01-09 20:13:27 +0800

    Initial Commit

---
+++ FitTracker/FitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitTrackerApp.swift
+//  FitTracker
+//
+//  Created by Jose Miguel on 1/9/26.
+//
+
+import SwiftUI
+
+@main
+struct FitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
